b8b2e0a5886a39cad91a1c16cbba03fbb19ff357
roshan2057 <roshankarki1276@gmail.com>
2023-07-19 19:15:04 +0545

    assign login and sigin pages

---
+++ src/components/pages/Signup/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './signup.css'
+
+const Signup = () => {
+  return (
+    <>
+    <div className='signup'>
+        Create a free account
+    </div>
+    </>
+  )
+}
+
+export default Signup
