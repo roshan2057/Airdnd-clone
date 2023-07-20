5cc56a62592c2989924518959fa96b8376787c91
Bishal1998 <Bishalpaudel1998@gmail.com>
2023-07-20 15:11:33 +0545

    Login UI

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
                 <p className='login-sign'>Don't have an account? <a href="/signup">Sign Up</a></p>
             </header>
             <div className='login-box'>
-                <form className='login-form'>
+                <form className='login-form' autocomplete>
                     <h2>Welcome to MarketMinder</h2>
                     <div className='login-form-top'>
                         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/753px-Google_%22G%22_Logo.svg.png' alt="Google Logo" width={23} height={23} />
@@ -21,10 +21,16 @@ const Login = () => {
                         <hr />
                     </div>
                     <div className='login-form-box'>
-                        <input type="text" />
-                        <input type="text" />
-                        <button>Sign In</button>
-                        <p>Forgot your password?</p>
+                        <div className='input-div' >
+                            <input type="text" />
+                            <p>Email</p>
+                        </div>
+                        <div className='input-div' >
+                            <input type="text" />
+                            <p>Password</p>
+                        </div>
+                        <button disabled={true}>Sign In</button>
+                        <p className='forgot'>Forgot your password?</p>
                     </div>
 
                 </form>
